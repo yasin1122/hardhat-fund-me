d868090557cc97ce22361cdce68aaa0d5069d849
Yasin Cakal <whoisyasin@gmail.com>
2023-02-27 14:29:16 +0300

    first

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,11 @@
+// const { hre } = require("ethers/lib/utils")
+
+// function deployFunc(hre) {
+//     console.log("Hi!")
+//     hre.getNamedAccounts()
+//     hre.deployments
+// }
+
+module.exports = async ({ getNamedAccounts, deployments}) => {
+
+}
